d603ed35b09298c36eae403d15e0b3dfec74ec5e
morais244 <morais.n@escolar.ifrn.edu.br>
2024-08-19 11:52:35 -0300

    script to player

---
+++ Meu Game 3D/Assets/scripts/jogador.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jogador : MonoBehaviour
+{
+    public int velocidade = 10;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+       TryGetComponent(out rb);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+        float h = Input.GetAxis("Horizontal"); // -1 esquerda, 0 nada, 1 direita
+        float v = Input.GetAxis("Vertical"); // -1 pra tras, 0 nada, 1 pra frente
+
+        
+        Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade);
+
+    }
+}
